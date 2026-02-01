57ffb76c4a26f1967856d23933051884ec88b14d
growgavin4-sudo <growgavin4@gmail.com>
2026-02-01 09:24:02 -0600

    Correct Magic8Ball response logic and formatting
    Fix syntax errors and improve response selection.

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -8,13 +8,17 @@ import random
 
 def main():
   #Create a list of your responses.
-  answers = ["It is decidedly so", "It is certain","Without a doubt", "Yes definitely","You may rely on it","As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again later","Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again". "Dont count on it", "My reply is no","My sources say no","Outlook not so good", "Very doubtful"]
+  answers=["It is decidedly so", "It is certain","Without a doubt", "Yes definitely","You may rely on it","As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again later","Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no","My sources say no","Outlook not so good", "Very doubtful"]
   length = len(answers)           
   print("Magic 8 Ball")
   #Prompt the user for their question.
 input ("What is your question?:")
   #Answer question randomly with one of the options from your earlier list.
-answers 
+length = len(answers)
 r = random.random() *length
+r = int()
+print (r)
+response = answers(r)
+print (response)
 if __name__ == '__main__':
   main()
